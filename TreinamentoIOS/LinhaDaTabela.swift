20edffba47acac768c916ecad6354572a63f62fe
Leandro Souza <leandro.silva@solutis.com.br>
2017-07-13 10:34:39 -0300

    Criação de classes

---
+++ TreinamentoIOS/LinhaDaTabela.swift
@@ -0,0 +1,17 @@
+//
+//  LinhaDaTabela.swift
+//  TreinamentoIOS
+//
+//  Created by Solutis on 7/12/17.
+//  Copyright © 2017 Solutis. All rights reserved.
+//
+
+import UIKit
+
+class LinhaDaTabela : UITableViewCell {
+    
+    @IBOutlet weak var addImage: UIImageView!
+    @IBOutlet weak var botao: UIButton!
+    
+    
+}
---
+++ TreinamentoIOS/Tabela.swift
@@ -0,0 +1,47 @@
+//
+//  Tabela.swift
+//  TreinamentoIOS
+//
+//  Created by Solutis on 7/12/17.
+//  Copyright © 2017 Solutis. All rights reserved.
+//
+
+import UIKit
+
+class Tabela : UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let nomes = ["nome1","nome2","nome3","nome4","nome5","nome6"]
+    
+    override func viewDidLoad() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+}
+
+extension Tabela : UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celula = tableView.dequeueReusableCell(withIdentifier: "linhaDaTabela") as? LinhaDaTabela
+        
+        celula!.addImage.image = UIImage(named: "add")
+        celula!.botao.setTitle(nomes[indexPath.row], for: .normal)
+        
+        return celula!
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nomes.count
+    }
+    
+}
+
+extension Tabela : UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("select")
+    }
+    
+}
