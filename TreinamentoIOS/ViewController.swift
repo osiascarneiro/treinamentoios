ee90fc04f1812685b46f6ceca6980598593653d2
Leandro Souza <leandro.silva@solutis.com.br>
2017-07-13 10:35:04 -0300

    Criação de classes

--- TreinamentoIOS/ViewController.swift
+++ TreinamentoIOS/ViewController.swift
@@ -9,17 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelTeste: UILabel!
 
+    @IBAction func click(_ sender: UIButton) {
+        performSegue(withIdentifier: "lol", sender: sender)
+        
+        let constraint = NSLayoutConstraint(item: labelTeste, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1, constant: 0)
+        
+        self.view.addConstraint(constraint)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        labelTeste.text = "Hello Wold"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("appear!!")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if(segue.identifier == "lol") {
+            if let destino = segue.destination as? ViewController2 {
+                destino.delegate = self
+            }
+        } else {
+            print("ops")
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension ViewController : ViewController2Delegate {
+    
+    func dismissController() {
+        self.dismiss(animated: false, completion: nil)
+        self.performSegue(withIdentifier: "teste", sender: nil)
     }
-
-
+    
 }
 
